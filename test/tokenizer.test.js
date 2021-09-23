92f209da83393f3d1ad2b4e57f05d1ff4b42330e
angelicagrass <angelicagrass@hotmail.com>
2021-09-23 11:00:59 +0200

    jest tesing

---
+++ test/tokenizer.test.js
@@ -0,0 +1,9 @@
+import { Tokenizer } from "../src/tokenizer"
+import { Grammar } from "../src/grammar"
+import { arithmeticRegex } from "./arithmeticTest"
+import { exclamationRegex } from "./exclamationTest"
+
+describe('Tests for tokenizer', () => {
+  
+  
+})
