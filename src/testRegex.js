60c8d76f8ed1f6f0b7cc9094adab436508e34d18
angelicagrass <angelicagrass@hotmail.com>
2021-09-23 08:57:59 +0200

    added regex to test with

---
+++ src/testRegex.js
@@ -0,0 +1,20 @@
+export const regex = [
+  {
+    TYPE: 'FLOAT',
+    REGEX: /^[0-9]+\.[0-9]+/g 
+  },
+  {
+    TYPE: 'INTEGER',
+    REGEX: /^[0-9]+/g , 
+  }
+
+  // {
+  //   TYPE: 'WORD',
+  //   REGEX: /^[\w|ÅÄÖåäö]+/g , 
+  // },
+  // {
+  //   TYPE: 'DOT',
+  //   REGEX: /^\./g 
+  // }
+]
+
